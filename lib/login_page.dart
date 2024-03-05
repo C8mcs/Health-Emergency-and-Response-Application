e63710491e8707d299dfa810b18c0ffb36bc7cb9
C8mcs <camucas@up.edu.ph>
2024-03-05 23:07:55 +0800

    added temp void func in login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,9 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+//TEMPORARY!!
+void main() {
+  runApp(LoginPage());
+}
 
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
