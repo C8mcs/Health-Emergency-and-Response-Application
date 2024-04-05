29586ee9889ec5908c20cc904b6ee95b9eaf1d1b
C8mcs <camucas@up.edu.ph>
2024-04-05 23:45:18 +0800

    added draggable/expandable user information container.

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -22,7 +22,7 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  //User Profile variableszs
+  // User Profile variables
   TextEditingController userHeight = TextEditingController();
   TextEditingController userWeight = TextEditingController();
   TextEditingController firstName = TextEditingController();
@@ -37,6 +37,9 @@ class _ProfilePageState extends State<ProfilePage> {
   TextEditingController userMedicalCond = TextEditingController();
   TextEditingController userAllergies = TextEditingController();
   TextEditingController userMedications = TextEditingController();
+
+  // Initial visibility of additional information
+  bool additionalInfoVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +56,7 @@ class _ProfilePageState extends State<ProfilePage> {
               color: Colors.black,
             ),
             onPressed: () {
-              print('IconButton pressed ...'); //Button function \^-^/??
+              print('IconButton pressed ...');
             },
           ),
         ),
@@ -61,154 +64,195 @@ class _ProfilePageState extends State<ProfilePage> {
         elevation: 4,
       ),
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.elliptical(100, 70), //tab curve???
-              bottomRight: Radius.circular(40),
+        child: GestureDetector(
+          onVerticalDragUpdate: (details) {
+            if (details.delta.dy > 0) {
+              setState(() {
+                additionalInfoVisible = true;
+              });
+            } else {
+              setState(() {
+                additionalInfoVisible = false;
+              });
+            }
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.elliptical(100, 70), //tab curve???
+                bottomRight: Radius.circular(40),
+              ),
             ),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'User Info',
-                  style: TextStyle(
-                    color: Color(0xFFD92B4B),
-                    fontSize: 20,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // Implement action to edit the image
+                          print('Edit image');
+                        },
+                        child: Container(
+                          width: 120,
+                          height: 120,
+                          child: Padding(
+                            padding: EdgeInsets.all(8),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.network(
+                                //update img acc to user profile
+                                'https://picsum.photos/seed/75/600',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Flexible(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                          child: TextFormField(
+                            controller: userHeight,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'^\d+\.?\d{0,2}')),
+                            ],
+                            keyboardType: TextInputType.number,
+                            autofocus: true,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              labelText: 'Height (cm)',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            validator: (value) {
+                              // Validation logic here
+                              return null;
+                            },
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                          child: TextFormField(
+                            controller: userWeight,
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.allow(
+                                  RegExp(r'^\d+\.?\d{0,2}')),
+                            ],
+                            keyboardType: TextInputType.number,
+                            autofocus: true,
+                            obscureText: false,
+                            decoration: InputDecoration(
+                              labelText: 'Weight (kg)',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.white,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.blue,
+                                  width: 2,
+                                ),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            validator: (value) {
+                              // Validation logic here
+                              return null;
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-                Row(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      width: 120,
-                      height: 120,
-                      child: Padding(
-                        padding: EdgeInsets.all(8),
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: Image.network(
-                            //update img acc to user profile
-                            'https://picsum.photos/seed/75/600',
-                            fit: BoxFit.cover,
+                  buildTextField('Firstname', firstName),
+                  buildTextField('Lastname', lastName),
+                  Visibility(
+                    visible: additionalInfoVisible,
+                    child: Column(
+                      children: [
+                        buildTextField('Age', userAge,
+                            keyboardType: TextInputType.number),
+                        buildTextField('Sex at Birth', userSex),
+                        buildTextField('Blood Type', userBloodType),
+                        buildTextField('Address', userAddress),
+                        Divider(
+                          thickness: 10,
+                        ),
+                        buildTextField(
+                            'Emergency Contact Person', emergencyContactName),
+                        buildTextField(
+                            'Emergency Contact Number', emergencyContactNumber,
+                            keyboardType: TextInputType.number),
+                        buildTextField('Emergency Contact Address',
+                            emergencyContactAddress),
+                        Divider(
+                          height: 20,
+                          thickness: 10,
+                        ),
+                        buildTextField('Medical Conditions:', userMedicalCond),
+                        buildTextField('Allergies:', userAllergies),
+                        buildTextField('Current Medication:', userMedications),
+                        SizedBox(height: 30),
+                        Container(
+                          child: Align(
+                            alignment: Alignment.centerRight,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Save changes made by user
+                                setState(() {
+                                  userHeight = userHeight;
+                                  userWeight = userWeight;
+                                  firstName = firstName;
+                                  lastName = lastName;
+                                  userAge = userAge;
+                                  userSex = userSex;
+                                  userBloodType = userBloodType;
+                                  userAddress = userAddress;
+                                  emergencyContactName = emergencyContactName;
+                                  emergencyContactNumber =
+                                      emergencyContactNumber;
+                                  emergencyContactAddress =
+                                      emergencyContactAddress;
+                                  userMedicalCond = userMedicalCond;
+                                  userAllergies = userAllergies;
+                                  userMedications = userMedications;
+                                });
+                                // Hide additional information when dragged up
+                                setState(() {
+                                  additionalInfoVisible = false;
+                                });
+                              },
+                              child: Text('Save Profile'),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
-                    Flexible(
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
-                        child: TextFormField(
-                          controller: userHeight,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.allow(
-                                RegExp(r'^\d+\.?\d{0,2}')),
-                          ],
-                          keyboardType: TextInputType.number,
-                          autofocus: true,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            labelText: 'Height (cm)',
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.blue,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          validator: (value) {
-                            // Validation logic here
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
-                        child: TextFormField(
-                          controller: userWeight,
-                          inputFormatters: <TextInputFormatter>[
-                            FilteringTextInputFormatter.allow(
-                                RegExp(r'^\d+\.?\d{0,2}')),
-                          ],
-                          keyboardType: TextInputType.number,
-                          autofocus: true,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            labelText: 'Weight (kg)',
-                            enabledBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.white,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            focusedBorder: UnderlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.blue,
-                                width: 2,
-                              ),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                          ),
-                          validator: (value) {
-                            // Validation logic here
-                            return null;
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                buildTextField('Firstname', firstName),
-                buildTextField('Lastname', lastName),
-                Row(
-                  children: [
-                    Expanded(
-                      child: buildTextField(
-                        'Age',
-                        userAge,
-                        keyboardType: TextInputType.number,
-                      ),
-                    ),
-                    SizedBox(width: 5),
-                    Expanded(child: buildTextField('Sex at Birth', userSex)),
-                    SizedBox(width: 5),
-                    Expanded(
-                        child: buildTextField('Blood Type', userBloodType)),
-                  ],
-                ),
-                buildTextField('Address', userAddress),
-                Divider(
-                  thickness: 10,
-                ),
-                buildTextField(
-                    'Emergency Contact Person', emergencyContactName),
-                buildTextField(
-                    'Emergency Contact Number', emergencyContactNumber,
-                    keyboardType: TextInputType.number),
-                buildTextField(
-                    'Emergency Contact Address', emergencyContactAddress),
-                Divider(
-                  height: 20,
-                  thickness: 10,
-                ),
-                buildTextField('Medical Conditions:', userMedicalCond),
-                buildTextField('Allergies:', userAllergies),
-                buildTextField('Current Medication:', userMedications),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
