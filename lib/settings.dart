ab3f410ada99251bbe5a99fe57d9fb9e019d59a1
Jezia Bethany Sabalilag <70994577+bitany@users.noreply.github.com>
2024-06-02 00:08:34 +0800

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -25,131 +25,100 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.only(left: 10),
+        padding: EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            SizedBox(height: 20,),
-            ElevatedButton(
-              onPressed: () {
+            SettingsMenuItem(
+              icon: Icons.person,
+              text: 'Change Username',
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ChangeUsernamePage()),
                 );
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Color(0xFFF24171),
-                shadowColor: Colors.black,
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(color: Color(0xFFF24171), width: 2.0),
-                ),
-              ),
-              child: Text(
-                'Change Username',
-                style: TextStyle(fontSize: 24),
-              ),
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(
-              onPressed: () {
+            SettingsMenuItem(
+              icon: Icons.lock,
+              text: 'Change Password',
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => ChangePasswordPage()),
                 );
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Color(0xFFF24171),
-                shadowColor: Colors.black,
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(color: Color(0xFFF24171), width: 2.0),
-                ),
-              ),
-              child: Text(
-                'Change Password',
-                style: TextStyle(fontSize: 24),
-              ),
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(
-              onPressed: () {
+            SettingsMenuItem(
+              icon: Icons.settings,
+              text: 'Preferences',
+              onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => PreferencesPage()),
                 );
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white,
-                foregroundColor: Color(0xFFF24171),
-                shadowColor: Colors.black,
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(color: Color(0xFFF24171), width: 2.0),
-                ),
-              ),
-              child: Text(
-                'Preferences',
-                style: TextStyle(fontSize: 24),
-              ),
             ),
-            SizedBox(height: 10,),
-            ElevatedButton(
-              onPressed: () {
+            SettingsMenuItem(
+              icon: Icons.logout,
+              text: 'Logout',
+              onTap: () {
                 showDialog(
                   context: context,
                   builder: (BuildContext context) {
-                    // Ask user for confirmation to logout
                     return AlertDialog(
                       title: Text('Confirm logout'),
                       content: Text('Are you sure you want to log out?'),
                       actions: [
-                        ElevatedButton(
+                        TextButton(
                           onPressed: () {
                             Navigator.of(context).pop(); // Close the dialog
                           },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFFFFFF),
-                          ),
-                          child: Text('Cancel', style: TextStyle(color: Colors.white),),
+                          child: Text('Cancel'),
                         ),
                         ElevatedButton(
                           onPressed: () {
-                            logout(context);  // go back to login page
+                            logout(context); // Perform logout
                           },
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFFBD2D29),
+                            backgroundColor: Colors.red,
                           ),
-                          child: Text('Logout', style: TextStyle(color: Colors.white),),
+                          child: Text('Logout'),
                         ),
                       ],
                     );
                   },
                 );
               },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFFF24171),
-                foregroundColor: Colors.white,
-                shadowColor: Colors.black,
-                elevation: 10.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                  side: BorderSide(color: Colors.white, width: 2.0),
-                ),
-              ),
-              child: Text(
-                'Logout',
-                style: TextStyle(fontSize: 24),
-              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SettingsMenuItem extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback onTap;
+
+  SettingsMenuItem({required this.icon, required this.text, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.black),
+        title: Text(text, style: TextStyle(fontSize: 18, color: Colors.black)),
+        trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+        onTap: onTap,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        tileColor: Colors.white,
+        contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
       ),
     );
   }
